655d6339f88d1e0896b924d4291b71ac2babef86
oimaster99 <oimaster99@gmail.com>
2024-05-08 12:26:07 -0600

    Added the Roy function

--- Big.py
+++ Big.py
@@ -4,4 +4,13 @@ def main():
 def ethan():
     print(f'Hello, I am Ethan from Big.py')
 
+def roy():
+    answer = input(f"Who is Joe?: ")
+    if (answer == "Joemama"):
+        print("good")
+        return True
+    else:
+        print("false")
+        return False
+
 main()
